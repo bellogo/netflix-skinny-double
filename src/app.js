9524715d5cfe5ba4ba54ad6fe821f6b7a424fbf5
unknown <ufuomaogodo@gmail.com>
2021-01-11 19:07:35 +0100

    Set-up development environment
    - set up babel
    - set up mongodb
    - set up server

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//connect to DB and Start server
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true,  useUnifiedTopology: true })
+.then((result) => app.listen(port, () => {
+    console.log(`DB connnected, Server Running on: ${port}`);
+}))
+.catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 app.get("/",(req,res) => {
     res.send("Welcome to netflix skinny double");
 })
 
-app.listen(port, () => {
-    console.log(`Server Running on: ${port}`);
-});
+
