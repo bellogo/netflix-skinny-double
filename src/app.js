b8eed7c7aade1d48c486bed6390216e951da6422
unknown <ufuomaogodo@gmail.com>
2021-01-12 11:25:57 +0100

    Setup eslint to lint codebase
    - install eslint
    - setup eslint configuration
    - use airbnb standards

--- src/app.js
+++ src/app.js
@@ -8,16 +8,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-//connect to DB and Start server
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true,  useUnifiedTopology: true })
-.then((result) => app.listen(port, () => {
+// connect to DB and Start server
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(port, () => {
     console.log(`DB connnected, Server Running on: ${port}`);
-}))
-.catch((err) => console.log(err));
+  }))
+  .catch(err => console.log(err));
 
 app.use(bodyParser.json());
-app.get("/",(req,res) => {
-    res.send("Welcome to netflix skinny double");
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Welcome to netflix skinny double");
+});
