a860dfede4b77b2353af3a77faff7837f6ef15f1
unknown <ufuomaogodo@gmail.com>
2021-03-02 06:50:52 +0100

    change movie model

--- src/models/movie.js
+++ src/models/movie.js
@@ -3,16 +3,44 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const MovieSchema = new Schema({
-  title: String,
-  genre: Array,
-  rating: Number,
-  description: String,
-  imgLink: String,
-  trailerLink: String,
-  watchLink: String,
-  downloadLq: String,
-  downloadHq: String,
-  watched: Boolean
+  title: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: [String],
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 10
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imgLink: {
+    type: String,
+    required: true,
+  },
+  trailerLink: {
+    type: String
+  },
+  watchLink: {
+    type: String
+  },
+  downloadLq: {
+    type: String
+  },
+  downloadHq: {
+    type: String
+  },
+  watched: {
+    type: String,
+    default: false
+  }
 });
 
 const Movie = mongoose.model("movie", MovieSchema);
